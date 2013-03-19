c3ff1c107d478a036e6814c3bcc3ea8619e4f753
Laura Leclercq <leclercq.laura@gmail.com>
2013-03-19 18:34:59 +0100

    ajout look and feel

--- src/implementation/FenetrePrincipale.java
+++ src/implementation/FenetrePrincipale.java
@@ -13,6 +13,8 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import facade.IFenetrePrincipale;
 
@@ -150,6 +152,21 @@ public class FenetrePrincipale implements IFenetrePrincipale {
 	}
 
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		new FenetrePrincipale();
 	}
 }
